3c26bca93330e9e823990644ca7d10c1c1cb309b
Mason <mgershenson@gmail.com>
2026-03-20 22:40:32 -0700

    Startup file for ROV - a utility for Mason

--- start_joy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /opt/ros/jazzy/setup.bash
-source ~/slvrov_ros/install/setup.bash
-
-gnome-terminal -- bash -c "ros2 run joy joy_node --ros-args -r /joy:=/joy_left -p device_id:=0; bash"
-gnome-terminal -- bash -c "ros2 run joy joy_node --ros-args -r /joy:=/joy_right -p device_id:=1; bash"
