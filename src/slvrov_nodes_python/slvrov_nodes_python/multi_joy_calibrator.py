45adea29e61ae8668b419ce34e86f5b81ed4d3e0
Caleb Hofschneider <calebhof11@gmail.com>
2026-03-24 10:32:55 -0700

    Delete src/slvrov_nodes_python/slvrov_nodes_python/multi_joy_calibrator.py

--- src/slvrov_nodes_python/slvrov_nodes_python/multi_joy_calibrator.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Compatibility wrapper for the renamed joystick calibrator module."""
-
-from slvrov_nodes_python.joystick_calibrator import JoystickCalibrator, main
-
-__all__ = ["JoystickCalibrator", "main"]
