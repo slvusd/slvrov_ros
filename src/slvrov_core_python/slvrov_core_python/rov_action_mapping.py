2cd94a33f28aef3f31b5b4bf953374a7b71af4de
LegionaryOfLogic <calebhof11@gmail.com>
2026-04-21 21:44:00 +0000

    started to reduce and edit JoystickMapper node
    moved non-joystick mapping tools to rov_action_mapping.py

---
+++ src/slvrov_core_python/slvrov_core_python/rov_action_mapping.py
@@ -0,0 +1,148 @@
+class ROVActionType(Enum):
+    """Enumerate the types of logical actions that can be calibrated."""
+
+    AXIS = "axis"
+    BUTTON = "button"
+
+
+class ROVAction(Enum):
+    """Enumerate the logical actions that can be calibrated."""
+
+    AXIS_FORWARD = "axis_forward"
+    AXIS_STRAFE = "axis_strafe"
+    AXIS_YAW = "axis_yaw"
+    AXIS_HEAVE = "axis_heave"
+    AXIS_ROLL = "axis_roll"
+    AXIS_PITCH = "axis_pitch"
+
+    AXIS_CLAW_OPEN = "axis_claw_open"
+    AXIS_CLAW_ROTATE = "axis_claw_rotate"
+    AXIS_CLAW_TILT = "axis_claw_tilt"
+    AXIS_CAMERA = "axis_camera"
+
+    BUTTON_FORWARD = "button_forward"
+    BUTTON_STRAFE = "button_strafe"
+    BUTTON_YAW = "button_yaw"
+    BUTTON_HEAVE = "button_heave"
+    BUTTON_ROLL = "button_roll"
+    BUTTON_PITCH = "button_pitch"
+
+    BUTTON_CLAW_OPEN = "button_claw_open"
+    BUTTON_CLAW_ROTATE = "button_claw_rotate"
+    BUTTON_CLAW_TILT = "button_claw_tilt"
+    BUTTON_CAMERA = "button_camera"
+
+
+@dataclass(frozen=True)
+class ControlMapping:
+    """
+    Record the binding from one physical control to one logical action.
+    Will be serialized to JSON for storage on disk. In most cases, loading the 
+    mappings from disk will be used just as a dict object, rather than converting
+    back into ControlMapping instances. 
+    This class is primarily for structuring the data while preforming Create and Update operations on the mappings.
+    """
+
+    action: ROVAction
+    topic: str
+    source: ROVActionType
+    index: int
+    invert: bool = False
+    deadzone: float = 0.1
+    scale: float = 1.0
+
+    def _prep_json(self) -> Dict:
+        """Return a JSON-serializable dict representation of this mapping."""
+        
+        return self.action, {"topic": self.topic, "source": self.source, "index": self.index, "invert": self.invert, "deadzone": self.deadzone, "scale": self.scale}
+
+
+from json import load, dump, JSONDecodeError
+
+
+def write_control_mappings(mappings: List[ControlMapping], path: Path | str, indent: int=2) -> None:
+    """Write the given mappings to a JSON file at the given path."""
+
+    if type(path) == str: path = Path(path)
+    path.touch(exist_ok=True)   
+
+    json_dict = {}
+
+    for mapping in mappings:
+        action, mapping_dict = mapping._prep_json()
+        json_dict[action] = mapping_dict
+
+    with open(path, "w") as file:
+        dump(json_dict, file, indent=indent)
+
+
+def append_control_mapping(mapping: ControlMapping, path: Path | str, indent: int=2) -> None:
+    """Append the given mapping to a JSON file at the given path."""
+
+    if type(path) == str: path = Path(path)
+    path.touch(exist_ok=True)
+
+    with open(path, "r+") as file:
+        try: mappings_json = load(file)
+        except JSONDecodeError: mappings_json = {}
+
+        for mapping in mappings_json.values():
+            action, mapping_json = mapping._prep_json()
+
+            if action in mappings_json: raise NameError(f"Action '{action}' already exists in the mapping file. Each action can only be mapped once.")
+            mappings_json[action] = mapping_json
+
+        file.seek(0)  # Move pointer back to the very start
+        dump(mappings_json, file, indent=indent)
+        file.truncate()  # Clean up any leftover old data
+
+
+def edit_control_mapping(mapping: ControlMapping, path: Path | str, indent: int=2) -> None:
+    """Edit the given mapping in a JSON file at the given path."""
+
+    if type(path) == str: path = Path(path)
+    path.touch(exist_ok=True)
+
+    with open(path, "r+") as file:
+        try: mappings_json = load(file)
+        except JSONDecodeError: mappings_json = {}
+
+        action, mapping_dict = mapping._prep_json()
+
+        if action not in mappings_json: raise NameError(f"Action '{action}' does not exist in the mapping file. Only existing actions can be edited.")
+        mappings_json[action] = mapping_dict
+
+        file.seek(0)  # Move pointer back to the very start
+        dump(mappings_json, file, indent=indent)
+        file.truncate()  # Clean up any leftover old data
+
+
+def delete_control_mapping(action: ROVAction, path: Path | str, indent: int=2) -> None:
+    """Delete the given mapping from a JSON file at the given path."""
+
+    if type(path) == str: path = Path(path)
+    path.touch(exist_ok=True)
+
+    with open(path, "r+") as file:
+        try: mappings_json = load(file)
+        except JSONDecodeError: mappings_json = {}
+
+        if action not in mappings_json: raise NameError(f"Action '{action}' does not exist in the mapping file. Only existing actions can be deleted.")
+        del mappings_json[action]
+
+        file.seek(0)  # Move pointer back to the very start
+        dump(mappings_json, file, indent=indent)
+        file.truncate()  # Clean up any leftover old data
+
+
+def get_control_mappings(path: Path | str) -> Dict[ROVAction, ControlMapping]:
+    """Read the control mappings from a JSON file at the given path."""
+
+    if type(path) == str: path = Path(path)
+    path.touch(exist_ok=True)
+
+    with open(path, "r") as file:
+        try: mappings_json = load(file)
+        except JSONDecodeError: mappings_json = {}
+
+        return mappings
